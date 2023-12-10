d9d59680840fe61e81f74af0f571481d83c2ed88
iospkg <iospkg@126.com>
2023-12-10 15:08:09 +0800

    hello update1

--- update.sh
+++ update.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
 dpkg-scanpackages -m ./debs > Packages
-bzip2 -c Packages > Packages.bz2
+xz -c9 Packages > Packages.xz
+bzip2 -c9 Packages > Packages.bz2
 gzip -c Packages > Packages.gz
+zstd -q -c19 Packages > Packages.zst
+
