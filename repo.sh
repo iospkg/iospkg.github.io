7679b6a083e469cd2be20a75edfdef2b0c8973fa
iospkg <iospkg@126.com>
2023-12-11 23:48:51 +0800

    hello update17

--- repo.sh
+++ repo.sh
@@ -5,13 +5,13 @@ cd $script_full_path || exit 1
 rm Packages Packages.bz2 Packages.xz Packages.zst Release Release.gpg
 
 echo "[Repository] Generating Packages..."
-apt-ftparchive packages ./pool > Packages
+./apt-ftparchive packages ./pool > Packages
 zstd -q -c19 Packages > Packages.zst
 xz -c9 Packages > Packages.xz
 bzip2 -c9 Packages > Packages.bz2
 
 echo "[Repository] Generating Release..."
-apt-ftparchive \
+./apt-ftparchive \
 		-o APT::FTPArchive::Release::Origin="Sileo" \
 		-o APT::FTPArchive::Release::Label="Sileo" \
 		-o APT::FTPArchive::Release::Suite="stable" \
@@ -22,7 +22,7 @@ apt-ftparchive \
 		-o APT::FTPArchive::Release::Description="Sileo for Checkra1n and Unc0ver" \
 		release . > Release
 
-echo "[Repository] Signing Release using Amy's GPG Key..."
-gpg -abs -u 816C7A50B575162DC29288CD72339224580758CE -o Release.gpg Release
+# echo "[Repository] Signing Release using Amy's GPG Key..."
+# gpg -abs -u 816C7A50B575162DC29288CD72339224580758CE -o Release.gpg Release
 
 echo "[Repository] Finished"
